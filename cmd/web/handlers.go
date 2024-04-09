36189701dd10ffc10f0fd2b3cabd9173298f9910
iamBharatManral <justbharatmanral@gmail.com>
2024-04-09 21:05:52 +0530

    ui: home page with header and logo

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,11 +1,23 @@
 package main
 
 import "net/http"
+import "log"
+import "html/template"
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Atom"))
+	ts, err := template.ParseFiles("./ui/html/home.tpl.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", Home)
+	mux.HandleFunc("/", home)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
